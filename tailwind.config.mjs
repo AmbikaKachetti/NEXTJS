992b9e83aa0af6de5f1710342de84f6f6f57432a
Ambika Rama Katyayini <35074062+AmbikaKachetti@users.noreply.github.com>
2025-01-20 13:38:25 +0530

    portfolio

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,27 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         lightHover: '#fcf4ff',
+//         darkHover: '#2a004a',
+//         darkTheme: '#11001F',
+//       },
+//       fontFamily:{
+//         Outfit: ["Outfit", "san-serif"],
+//         Ovo: ["Ovo", "serif"],
+//       }
+//     },
+//   },
+//   plugins: [],
+// };
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,11 +34,25 @@ export default {
         darkHover: '#2a004a',
         darkTheme: '#11001F',
       },
-      fontFamily:{
-        Outfit: ["Outfit", "san-serif"],
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
         Ovo: ["Ovo", "serif"],
+      },
+      animation: {
+        wave: 'wave 2s infinite',
+      },
+      keyframes: {
+        wave: {
+          '0%, 100%': { transform: 'rotate(0deg)' },
+          '50%': { transform: 'rotate(15deg)' },
+        },
+      },
+      boxShadow:{
+        'black': '4px 4px 0 #000',
+        'white': '4px 4px 0 #fff',
       }
     },
   },
   plugins: [],
 };
+
